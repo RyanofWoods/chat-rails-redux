5d21b6bfeab74b37a4d7786a17ff6aecf308490b
Ryan Woods <76776099+RyanofWoods@users.noreply.github.com>
2021-04-12 15:21:27 +0200

    Update app/models/channel.rb

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -2,7 +2,7 @@ class Channel < ApplicationRecord
   validates :name, presence: true
   validates :name, uniqueness: { case_sensitive: false }
   validates :name, length: { in: 3..15 }
-  validates :name, format: { with: /\A(\w|-)+\z/, message: "%{value} must only contain letters, numbers, dashes or underscores" } # any letter, number, dashes or underscores
+  validates :name, format: { with: /\A(\w|-)+\z/, message: "%{value} must only contain letters, numbers, dashes or underscores" }
 
   has_many :messages, dependent: :destroy
   has_many :users, through: :messages
