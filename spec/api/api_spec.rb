86e2a69a23a75725e0619fd91ec9864576e72fff
Ryan Woods <contact@ryanofwoods.com>
2021-04-14 10:54:47 +0200

    add spec file for API with tests for #get request

---
+++ spec/api/api_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe "API", type: :request do
+  def channel_with_messages(message_count: 20, channel_name: 'test_channel')
+    FactoryBot.create(:channel, name: channel_name) do |channel|
+      FactoryBot.create_list(:message, message_count, channel: channel)
+    end
+  end
+
+  let!(:channel) { channel_with_messages() }
+
+  # '/api/v1/:channel_name'
+  before { get '/api/v1/test_channel' }
+
+  context 'handles GET request' do
+    it 'returns status code 200' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'returns all messages from the #test_channel channel' do
+      expect(JSON.parse(response.body).size).to eq(20)
+    end 
+  end
+end
+
+## FUTURE:
+# update a message
+# delete a message
--- spec/factories/channel.rb
+++ spec/factories/channel.rb
@@ -2,6 +2,6 @@ require 'faker'
 
 FactoryBot.define do
   factory :channel do
-    name { Faker::Lorem.characters(number: 10) }
+    name { Faker::Lorem.characters(number: 8) }
   end
 end
