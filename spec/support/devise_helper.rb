4fc2fc485573490cb7da3dc2f62550cb449c83c4
Ryan Woods <contact@ryanofwoods.com>
2021-04-14 18:37:23 +0200

    add devise helpers to easily login for tests

---
+++ spec/support/devise_helper.rb
@@ -0,0 +1,10 @@
+module DeviseHelper
+  def login(user)
+    post user_session_path, params: {
+      user: {
+        email: user.email, password: user.password
+      }
+    }
+    follow_redirect!
+  end
+end
