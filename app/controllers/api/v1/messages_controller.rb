2872b091aaeba6b96606efa2b3f250515c612f48
Ryan Woods <contact@ryanofwoods.com>
2021-04-15 19:20:11 +0200

    add application policy for pundit and add post functionality (Passes POST request)

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,4 +1,7 @@
-class Api::V1::MessagesController < ApplicationController
+require 'pry-byebug'
+
+class Api::V1::MessagesController < Api::V1::BaseController
+  acts_as_token_authentication_handler_for User, except: [ :index, :show ]
   before_action :set_channel, only: [ :index, :create ]
 
   def index
@@ -6,11 +9,31 @@ class Api::V1::MessagesController < ApplicationController
   end
 
   def create
+    @message = Message.new(message_params)
+    @message.user = current_user
+    @message.channel = @channel
+
+    authorize @message
+
+    if @message.save
+      render :index
+    else
+      render_error
+    end
   end
 
   private
 
   def set_channel
     @channel = Channel.find_by(name: params[:channel_id])
+  end
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
+
+  def render_error
+    render json: { errors: @message.errors.full_messages },
+      status: :unprocessable_entity
   end
 end
