87f04a9eb3a19152eb7371cf362b2850d73cf94d
Ryan Woods <contact@ryanofwoods.com>
2021-04-22 19:46:52 +0200

    make message component pass unit tests

--- app/javascript/chat/components/message.jsx
+++ app/javascript/chat/components/message.jsx
@@ -2,12 +2,22 @@ import React from 'react';
 
 const Message = (props) => {
   const { author, content, created_at } = props.message;
-  const date = new Date(created_at).toLocaleTimeString();
+
+  const formattedTime = new Intl.DateTimeFormat("en-GB", {
+    timeStyle: "short",
+  }).format(created_at);
+
+  const formattedDate = new Intl.DateTimeFormat("en-GB", {
+    dateStyle: "short",
+  }).format(created_at);
+
+  const formattedDateStr = `${formattedTime}, ${formattedDate}`;
+
   return (
     <div className="message">
       <div className="message-info">
         <div className="message-author">{author}</div>
-        <div className="message-date">{date}</div>
+        <div className="message-date">{formattedDateStr}</div>
       </div>
       <div className="message-content">{content}</div>
     </div>
