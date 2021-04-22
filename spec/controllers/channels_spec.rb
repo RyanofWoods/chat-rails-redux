a6b77af0490dcca3a221d70e8a185be36d7419c3
Ryan Woods <contact@ryanofwoods.com>
2021-04-22 12:23:00 +0200

    add #show test to create general channel if it does not exist

--- spec/controllers/channels_spec.rb
+++ spec/controllers/channels_spec.rb
@@ -12,5 +12,16 @@ RSpec.describe ChannelsController, type: :controller do
 
       expect(response).to redirect_to("/channels/general")
     end
+
+    it "if there are no channels it should create a general channel then redirect to it" do
+      Channel.destroy_all
+
+      sign_in!
+      get :show
+
+      expect(Channel.count).to eq(1)
+      expect(Channel.first.name).to eq("general")
+      expect(response).to redirect_to("/channels/general")
+    end
   end
 end
