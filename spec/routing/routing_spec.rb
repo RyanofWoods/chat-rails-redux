f9726ae9b0d3d6ab0110419cae7499246c276aeb
Ryan Woods <contact@ryanofwoods.com>
2021-04-22 12:03:02 +0200

    add another routing test for channels#show

--- spec/routing/routing_spec.rb
+++ spec/routing/routing_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 RSpec.describe "Routing", type: :routing do
   it "should always redirect root(/) to channels#show" do
-    FactoryBot.create(:channel)
-    FactoryBot.create(:channel, name: 'general')
-
     expect(get("/")).to route_to("channels#show")
+  end
+
+  it "'/channels/test' should go to channels#show and id be 'test'" do
+    expect(get("/channels/test")).to route_to("channels#show", id: "test")
   end
 end
